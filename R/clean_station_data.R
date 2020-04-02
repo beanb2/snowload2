7e886ab32a4648d4ebc54e7d4596b0b10b8eff88
jadonwagstaff <jadonw@gmail.com>
2020-04-01 18:51:10 -0600

    Add maximum outlier and summer month filter.

--- R/clean_station_data.R
+++ R/clean_station_data.R
@@ -17,7 +17,25 @@
 #'     \item{}{\emph{"qflag"} - Removes all rows with a non-empty quality flag.
 #'     See ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/daily/readme.txt, section 3
 #'     for a description of the flag.}
+#'     \item{}{\emph{"month"} - Removes all rows with a month in the
+#'     \emph{months} parameter. Typically summer months are removed.}
+#'     \item{}{\emph{"high_max"} - Removes all rows where the value exceeds
+#'     the 3rd quartile plus \emph{iqr_cutoff} * the interquartile range of
+#'     the yearly maximums for each station. Alternatively, yearly maximums
+#'     can be found for clusters of stations by changing \emph{max_grouping}
+#'     to the name of the cluster ID column. If there are
+#'     \emph{max_yearly_outliers} or more values above the cutoff for a year
+#'     and station, then the values are retained.}
 #'   }
+#' @param months The months to remove when "month" is a part of \emph{clean}.
+#'   The default is 7 (Jul), 8 (Aug), and 9 (Sep).
+#' @param max_grouping When "high_max" is part of \emph{clean}, this specifies
+#'   what column to group by when finding interquartile ranges.
+#' @param iqr_cutoff The number of interquartile ranges above the 3rd quartile
+#'   to consider values outliers when "high_max" is part of \emph{clean}.
+#' @param max_yearly_outliers If there are this many values for a year and
+#'   station which are marked as maximum outliers when "high_max" is part of
+#'   \emph{clean}, the values are considered as not outliers.
 #'
 #' @return A data.frame where each row is a measurement of a climate variable
 #'   in elem for a single day and station. Columns are:
@@ -48,17 +66,69 @@
 #'
 #'
 #' @export
-clean_station_data <- function(station_data, clean = "all", report = FALSE) {
+clean_station_data <- function(station_data, clean = "all", report = FALSE,
+                               months = c(7, 8, 9), max_grouping = ID,
+                               iqr_cutoff = 3, max_yearly_outliers = 5) {
+  `%>%` <- magrittr::`%>%`
+
   # Missing presumed zero flag removal
   #=============================================================================
-  if ("mpzero" %in% clean) {
-    station_data <- dplyr::filter(station_data, MFLAG != "P" | is.na(MFLAG))
+  if ("mpzero" %in% clean || clean == "all") {
+    station_data <- station_data %>%
+      dplyr::filter(MFLAG != "P" | is.na(MFLAG))
   }
 
   # Quality flag removal
   #=============================================================================
-  if ("qflag" %in% clean) {
-    station_data <- dplyr::filter(station_data, QFLAG %in% c("", " ", NA))
+  if ("qflag" %in% clean || clean == "all") {
+    station_data <- station_data %>%
+      dplyr::filter(QFLAG %in% c("", " ", NA))
+  }
+
+  # Month removal
+  #=============================================================================
+  if ("month" %in% clean || clean == "all") {
+    station_data <- station_data %>%
+      dplyr::filter(!lubridate::month(DATE) %in% months)
+  }
+
+  # Max outlier removal
+  #=============================================================================
+  if ("high_max" %in% clean || clean == "all") {
+    max_grouping <- dplyr::enquo(max_grouping)
+
+    # year needed for finding maximums and number of yearly outliers
+    outliers <- station_data %>%
+      dplyr::mutate(MONTH = as.numeric(lubridate::month(DATE)),
+                    YEAR = as.numeric(lubridate::year(DATE)),
+                    YEAR = dplyr::if_else(MONTH > 9, YEAR, YEAR + 1))
+
+    # find a cutoff for each station based on the stations interquartile range
+    # of yearly maximums
+    high_cutoff <- outliers %>%
+      dplyr::group_by(!! max_grouping, YEAR) %>%
+      dplyr::summarise(MAX = dplyr::if_else(max(VALUE) == 0,
+                                            0,
+                                            log(max(VALUE)))) %>%
+      dplyr::group_by(!! max_grouping) %>%
+      dplyr::summarise(Q75 = stats::quantile(MAX, 0.75),
+                       IQR = Q75 - stats::quantile(MAX, 0.25),
+                       CUTOFF = dplyr::if_else(IQR < 0.001,
+                                               max(MAX),
+                                               Q75 + iqr_cutoff * IQR))
+
+    # find which values are above the cutoff, keep the values if there are
+    # enough for a given year
+    outliers <- suppressMessages(dplyr::left_join(outliers, high_cutoff)) %>%
+      dplyr::filter(log(VALUE) > CUTOFF) %>%
+      dplyr::group_by(!! max_grouping, YEAR) %>%
+      dplyr::filter(dplyr::n() < max_yearly_outliers) %>%
+      dplyr::ungroup()
+
+    # remove the detected outliers
+    station_data <- suppressMessages(
+      dplyr::anti_join(station_data, outliers)
+    )
   }
 
   return(station_data)
